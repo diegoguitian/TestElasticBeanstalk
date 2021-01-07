17053efb8f7ab6d89f95b61359a43bf7d36e05c2
Diego Guitian <guitian.diego@gmail.com>
2021-01-07 20:06:05 -0300

    add server file

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-const http = require('http');
-http.createServer(function(req, res) {
-    res.write('<h1>Elastic Beanstalk</h1>');
-    res.end();
-}).listen(3000);
+require('../TestElasticBeanstalk/server');
