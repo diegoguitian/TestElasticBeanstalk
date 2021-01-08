9033d27deaf05066036bbfd92380f4e7f91092a6
Diego Guitian <guitian.diego@gmail.com>
2021-01-07 21:24:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const { PORT = 3000 } = process.env;
 
 app.get('/', function(req, res) {
     res.send('<h1>TestElasticBeanstal</h1>')
 });
 
-app.listen(443);
+app.listen(PORT);
